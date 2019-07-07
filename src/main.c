874dd6e1ec8db42f0dd8a3e40de7b8bd595aea17
Adrien LENGLET <adrien.lenglet3@gmail.com>
2019-07-07 11:10:37 +0200

    Add additional 256KB RAM test

--- src/main.c
+++ src/main.c
@@ -26,9 +26,57 @@
 static status = 1;
 static jmp_buf main_end;
 
+static void * HiddenRAM(void){    // Check HiddenRAM
+    volatile unsigned int *NorRAM=(volatile unsigned int*)0xA8000000;    // Nomarl RAM TOP (no cache area)
+    volatile unsigned int *HidRAM=(volatile unsigned int*)0x88040000;    // Hidden RAM TOP (cache area)
+    int a,b;
+    int K55=0x55555555;
+    int KAA=0xAAAAAAAA;
+    char * HidAddress=NULL;
+
+    a= *NorRAM;
+    b= *HidRAM;
+    *NorRAM=K55;
+    *HidRAM=KAA;
+    if ( *NorRAM != *HidRAM ) {
+            HidAddress=(char*)HidRAM;    // Hidden RAM Exist
+    }
+    *NorRAM=a;
+    *HidRAM=b;
+    return HidAddress;
+}
+
+static void hidden_ram_test(void)
+{
+	size_t i;
+	char *ptr = HiddenRAM();
+
+
+	if (ptr == NULL) {
+		terminal_flush();
+
+		printf_term("No additionnal RAM found.\n");
+
+		terminal_show();
+		exit(0);
+	}
+	for (i = 0; i < 256000; i++)
+		ptr[i] = 70;
+	for (i = 0; i < 256000; i++)
+		if (ptr[i] != 70) {
+			terminal_flush();
+
+			printf_term("Unreliable additionnal RAM.\n");
+
+			terminal_show();
+			exit(0);
+		}
+}
+
 int AddIn_main(int isAppli, unsigned short OptionNum)
 {
 	if (setjmp(main_end) == 0) {
+		hidden_ram_test();
 		//CCompiler("\\\\crd0\\TEST.c");
 		CCompiler("\\\\fls0\\TEST.c");
 
